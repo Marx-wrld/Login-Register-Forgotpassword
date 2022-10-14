3c8a87392f7b27a2bf5750def64e60e315045399
Ian Marx <105711066+Marx-wrld@users.noreply.github.com>
2022-10-14 10:22:44 +0300

    Add files via upload

---
+++ LoginRegister/login.php
@@ -0,0 +1,48 @@
+<?php require_once "server.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form login-form">
+                <form action="login.php" method="POST">
+                    <h2 class="text-center">Login</h2>
+                    <p class="text-center">Login with your email and password.</p>
+                    <?php
+                    if(count($errors) > 0){
+                        ?>
+                        <div class="alert alert-danger text-center">
+                            <?php
+                            foreach($errors as $showerror){
+                                echo $showerror;
+                            }
+                            ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <div class="form-group">
+                        <input class="form-control" type="email"  autocomplete=""  name="email" placeholder="Email Address" required value="<?php echo $email ?>">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="password" autocomplete="" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="login" value="Login">
+                    </div>
+                    <br>
+                    <div class="link forget-pass text-center">Forgot password? <a href="forgot-password.php"> Reset here</a></div>
+                    <div class="link login-link text-center">Not yet a member? <a href="register.php">Register now</a></div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ LoginRegister/register.php
@@ -0,0 +1,64 @@
+<?php require_once "server.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register Form</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form">
+                <form action="login.php" method="POST">
+                    <h2 class="text-center">Register</h2>
+                    <p class="text-center">It's quick and easy.</p>
+                    <?php
+                    if(count($errors) == 1){
+                        ?>
+                        <div class="alert alert-danger text-center">
+                            <?php
+                            foreach($errors as $showerror){
+                                echo $showerror;
+                            }
+                            ?>
+                        </div>
+                        <?php
+                    }elseif(count($errors) > 1){
+                        ?>
+                        <div class="alert alert-danger">
+                            <?php
+                            foreach($errors as $showerror){
+                                ?>
+                                <li><?php echo $showerror; ?></li>
+                                <?php
+                            }
+                            ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <div class="form-group">
+                        <input class="form-control" type="text" name="name" placeholder="Full Name" required value="<?php echo $name ?>">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="email" name="email" placeholder="Email Address" required value="<?php echo $email ?>">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="password" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="password" name="cpassword" placeholder="Confirm password" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="register" value="Register">
+                    </div>
+                    <div class="link login-link text-center">Already a member? <a href="login.php">Login here</a></div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
